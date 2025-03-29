030827ba95761580cbbc34680863cbdbd3dfb9dd
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-03-30 05:58:36 +0600

    initial commit

---
+++ src/test/java/com/kolu/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolu.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
