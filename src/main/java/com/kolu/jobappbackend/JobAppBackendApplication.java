f15f8f78c8b061ae60e425a04d00fbe6030f3227
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-03-30 10:37:57 +0600

    initial commit

---
+++ src/main/java/com/kolu/jobappbackend/JobAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kolu.jobappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kolu/jobappbackend/JobAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolu.jobappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
