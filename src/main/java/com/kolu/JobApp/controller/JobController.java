35ecd34921927e393625d12bcd8cda75d24edd7d
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-03-30 08:50:06 +0600

    add viewAllJobs endpoint and corresponding JSP for job listings

--- src/main/java/com/kolu/JobApp/controller/JobController.java
+++ src/main/java/com/kolu/JobApp/controller/JobController.java
@@ -4,6 +4,7 @@ import com.kolu.JobApp.model.JobPost;
 import com.kolu.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -28,5 +29,11 @@ public class JobController {
     public String handleForm(JobPost jobPost) {
         jobService.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("/viewalljobs")
+    public String viewAllJobs(Model model) {
+        model.addAttribute("jobPosts", jobService.getAllJobs());
+        return "viewalljobs";
     }
 }
--- src/main/webapp/views/viewalljobs.jsp
+++ src/main/webapp/views/viewalljobs.jsp
@@ -2,8 +2,8 @@
 <%@ taglib prefix="ab" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page isELIgnored="false"%>
 <%@ page import="java.util.List" %>
-<%@ page import="com.telusko.JobApp.model.JobPost" %>
-<%@ page import="com.telusko.JobApp.repo.JobRepo" %>
+<%@ page import="com.kolu.JobApp.model.JobPost" %>
+<%@ page import="com.kolu.JobApp.repo.JobRepo" %>
 <!DOCTYPE html>
 <html lang="en">
 <head>
