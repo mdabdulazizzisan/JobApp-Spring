e33a44b9bfbe35ad146a80328b7c9e05862891b2
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-03-30 06:34:32 +0600

    add JobController with home endpoint

---
+++ src/main/java/com/kolu/JobApp/controller/JobController.java
@@ -0,0 +1,14 @@
+package com.kolu.JobApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @RequestMapping({"/", "/home"})
+    public String home() {
+        System.out.println("into Home Page");
+        return "home";
+    }
+}
