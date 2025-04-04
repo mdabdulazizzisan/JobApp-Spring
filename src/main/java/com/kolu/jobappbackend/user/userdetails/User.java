c633e6380364fe8dca7c349beb712aebe988b358
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-04 08:21:19 +0600

    implement methods of UserDetails class

--- src/main/java/com/kolu/jobappbackend/user/userdetails/User.java
+++ src/main/java/com/kolu/jobappbackend/user/userdetails/User.java
@@ -7,13 +7,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity(name = "_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
@@ -22,4 +27,39 @@ public class User {
     private String firstname;
     private String lastname;
     private Roles roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
